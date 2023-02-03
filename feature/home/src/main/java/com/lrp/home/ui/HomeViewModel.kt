53694b531b9277fe8c987ffcfe0535cb45ae0ad3
Max <max@MacBook-Pro.lan>
2023-02-03 19:19:51 +0200

    Initial commit

---
+++ feature/home/src/main/java/com/lrp/home/ui/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.lrp.home.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
