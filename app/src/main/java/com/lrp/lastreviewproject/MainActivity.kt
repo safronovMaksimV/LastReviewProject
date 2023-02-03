bdef714670b878d9a570b681fdb7cf840fd0f51c
safronovMaksim1 <82446390+safronovMaksim1@users.noreply.github.com>
2023-02-03 20:10:40 +0200

    Update MainActivity.kt

--- app/src/main/java/com/lrp/lastreviewproject/MainActivity.kt
+++ app/src/main/java/com/lrp/lastreviewproject/MainActivity.kt
@@ -12,6 +12,8 @@ import com.lrp.lastreviewproject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
